323c8cf3177bff76433036affa0af5099ad4a734
Tony Han <h.bing612@gmail.com>
2018-01-28 23:52:39 +0800

    add callings to format_string

--- lib/escript_export.ex
+++ lib/escript_export.ex
@@ -1,5 +1,8 @@
 defmodule EscriptExport do
   def main(_) do
+    IO.inspect(function_exported?(Code, :format_string!, 1))
     IO.inspect(function_exported?(Code, :format_string!, 2))
+    IO.inspect(Code.format_string!("a=1"))
+    IO.inspect(Code.format_string!("a=1"), file: "test")
   end
 end
